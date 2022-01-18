90fb2e3d14682e2071c9aff7089f8ba8c7a9ee66
Nathanael Carauna <nathanael.carauna@gmail.com>
2022-01-18 13:00:33 -0300

    Handling  invalid positions

--- ConsoleChessGame/src/boardgame/Board.java
+++ ConsoleChessGame/src/boardgame/Board.java
@@ -10,7 +10,7 @@ public class Board {
      * @param columns
      */
     public Board(int rows, int columns) {
-        if(rows < 1 || columns < 1){
+        if (rows < 1 || columns < 1) {
             throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
         }
         this.rows = rows;
@@ -34,58 +34,77 @@ public class Board {
 
     /**
      * Return piece at the specified row and column
+     * 
      * @param row
      * @param column
      * @return
      */
-    public Piece piece(int row, int column){
+    public Piece piece(int row, int column) {
+        if (!positionExists(row, column)) {
+            throw new BoardException("Position not on the board");
+        }
         return pieces[row][column];
     }
 
     /**
      * Return piece at the specified position
+     * 
      * @param position
      * @return
      */
-    public Piece piece(Position position){
+    public Piece piece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     /**
-     * Places piece at the specified position of the board and set placed piece position to the position specified
+     * Places piece at the specified position of the board and set placed piece
+     * position to the position specified
+     * 
      * @param piece
      * @param position
      */
-    public void placePiece(Piece piece, Position position){
+    public void placePiece(Piece piece, Position position) {
+        if (thereIsAPiece(position)) {
+            throw new BoardException("There is already a piece on position " + position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
 
     /**
      * Checks if given row and column exists on the board;
+     * 
      * @param row
      * @param column
      * @return
      */
-    public boolean positionExists(int row, int column){
-        return row >= 0 && row < this.rows && column >=0 && column < columns;
+    public boolean positionExists(int row, int column) {
+        return row >= 0 && row < this.rows && column >= 0 && column < columns;
     }
 
     /**
      * Check if position exists on the board;
+     * 
      * @param position
      * @return
      */
-    public boolean positionExists(Position position){
+    public boolean positionExists(Position position) {
         return positionExists(position.getRow(), position.getColumn());
     }
 
     /**
      * Checks if there is a piece at the given position
+     * 
      * @param position
      * @return
      */
-    public boolean thereIsAPiece(Position position){
+    public boolean thereIsAPiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
         return pieces[position.getRow()][position.getColumn()] != null;
     }
 }
