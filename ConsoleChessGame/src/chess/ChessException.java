715ae5fd1188383db38463bd1a078acb8ba1b04e
Nathanael Carauna <nathanael.carauna@gmail.com>
2022-01-18 21:17:03 -0300

    Chess exception created

---
+++ ConsoleChessGame/src/chess/ChessException.java
@@ -0,0 +1,13 @@
+package chess;
+
+public class ChessException extends RuntimeException {
+
+    /**
+     * @param message
+     */
+    public ChessException(String message) {
+        super(message);
+    }
+    
+    
+}
