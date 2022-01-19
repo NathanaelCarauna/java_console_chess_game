ee8efb14426c3a7e7054134d770bd75fda1fda35
Nathanael Carauna <nathanael.carauna@gmail.com>
2022-01-18 21:45:53 -0300

    Using ChessPosition to instantiate new pieces

--- ConsoleChessGame/src/chess/ChessMatch.java
+++ ConsoleChessGame/src/chess/ChessMatch.java
@@ -32,11 +32,22 @@ public class ChessMatch {
     }
 
     /**
+     * Places a chessPiece on board using chess coordinates ( ex: a1, b4, c6)
+     * 
+     * @param column
+     * @param row
+     * @param piece
+     */
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
+    /**
      * Set the initial setup of the pieces on the board
      */
     private void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
     }
 }
